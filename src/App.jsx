806ac6378c97d9cbec89ca43beba3f8306ef2093
JohanMarcano <johand503@gmail.com>
2024-08-11 21:53:38 -0400

    Creado Hito2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import Footer from './components/Footer';
+import RegisterPage from './components/RegisterPage'; // Importa el componente RegisterPage
+import LoginPage from './components/LoginPage'; // Importa el componente LoginPage
 
-function App() {
+const App = () => {
   return (
-    <>
+    <div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      {/*<RegisterPage />*/} 
+      <LoginPage />
       <Footer />
-    </>
+    </div>
   );
-}
+};
 
 export default App;
